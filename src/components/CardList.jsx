a4f56c2a8dc23345417cc9e1cdb4b162e16a3851
navya053-byte <navyasri.are@slu.edu>
2025-05-07 19:32:18 +0000

    Lab-07

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react'
+@@ -2,19 +2,16 @@ import React, { useState, useEffect } from 'react'
 import Card from './Card'
 import Button from './Button'
 import Search from './Search'
+import { BASE_URL } from '../config';
 
 const CardList = ({ data }) => {
+const CardList = () => {
   // define the limit state variable and set it to 10
   const limit = 10;
 
@@ -15,36 +17,27 @@ const CardList = ({ data }) => {
   useEffect(() => {
     setProducts(data.slice(offset, offset + limit));
   }, [offset, limit, data])
+  const [products, setProducts] = useState([]);
 
   const filterTags = (tagQuery) => {
     const filtered = data.filter(product => {
-      if (!tagQuery) {
-        return product
-      }
-
-      return product.tags.find(({title}) => title === tagQuery)
-    })
-
-    setOffset(0)
+@@ -29,6 +26,19 @@ const CardList = ({ data }) => {
     setProducts(filtered)
   }
+
+  const fetchProducts = () => {
+    fetch(`${BASE_URL}/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  };
+
+
+  useEffect(() => {
+    fetchProducts();
+  }, [offset]);
 
 
   return (
     <div className="cf pa2">
-      <Search handleSearch={filterTags}/>
-      <div className="mt2 mb2">
-      {products && products.map((product) => (
-          <Card key={product._id} {...product} />
-        ))}
-      </div>
-
-      <div className="flex items-center justify-center pa4">
-        <Button text="Previous" handleClick={() => setOffset(offset - limit)} />
-        <Button text="Next" handleClick={() => setOffset(offset + limit)} />
-      </div>
-    </div>
-  )
-}
-
-export default CardList;
